ec49fcebad06d8fd13be2e680b82179689440868
Yacine Tarezouket <y.tarezouket@gmail.com>
2023-11-20 14:37:45 +0100

    rails new

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,17 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    #@userinput = params[:userinput]
+    @answer = ["Great !", "Silly question, get dressed and go to work!", "I don't care, get dressed and go to work!"]
+    if params[:userinput] == 'I am going to work'
+      @answer = @answer[0]
+    elsif params[:userinput].include?('?')
+      @answer = @answer[1]
+    else params[:userinput] != 'I am going to work'
+      @answer = @answer[2]
+    end
+  end
+end
